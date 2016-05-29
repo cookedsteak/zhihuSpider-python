c06490e4661f42b72d1548cdf66e4a1c89fa2577
郭睿立 <cookedsteak708@gmail.com>
2016-05-29 08:36:26 +0800

    add connection

---
+++ connection.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class Conn(object):
+
+    db_config = 'mysql+pymysql://root:@localhost:3306/zhihu?charset=utf8'
+
+    session = None
+
+    def __init__(self):
+        if self.session is None:
+            config = self.db_config
+            engine = create_engine(config)
+            DBSession = sessionmaker(bind=engine)
+            self.session = DBSession()
+        else:
+            pass
+
+    def __del__(self):
+        if self.session is not None:
+            self.session.close()
+        else:
+            pass
+
+
